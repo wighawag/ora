2768e75126bbe328e9efe8d2f6d32e6585eb2e8b
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-02 19:24:20 +0000

    Add TypeScript definition (#101)

---
+++ index.test-d.ts
@@ -0,0 +1,48 @@
+import {expectType} from 'tsd-check';
+import {PassThrough} from 'stream';
+import ora, {promise} from '.';
+
+const spinner = ora('Loading unicorns');
+ora({text: 'Loading unicorns'});
+ora({spinner: 'squish'});
+ora({spinner: {frames: ['-', '+', '-']}});
+ora({spinner: {interval: 80, frames: ['-', '+', '-']}});
+ora({color: 'cyan'});
+ora({hideCursor: true});
+ora({indent: 1});
+ora({interval: 80});
+ora({stream: new PassThrough()});
+ora({isEnabled: true});
+
+spinner.color = 'yellow';
+spinner.text = 'Loading rainbows';
+expectType<boolean>(spinner.isSpinning);
+spinner.spinner = 'dots';
+spinner.indent = 5;
+
+spinner.start();
+spinner.start('Test text');
+spinner.stop();
+spinner.succeed();
+spinner.succeed('fooed');
+spinner.fail();
+spinner.fail('failed to foo');
+spinner.warn();
+spinner.warn('warn foo');
+spinner.info();
+spinner.info('info foo');
+spinner.stopAndPersist();
+spinner.stopAndPersist({text: 'all done'});
+spinner.stopAndPersist({symbol: '@', text: 'all done'});
+spinner.clear();
+spinner.render();
+spinner.frame();
+
+const resolves = Promise.resolve(1);
+promise(resolves, 'foo');
+promise(resolves, {
+	stream: new PassThrough(),
+	text: 'foo',
+	color: 'blue',
+	isEnabled: true
+});
